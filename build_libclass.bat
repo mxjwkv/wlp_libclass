ab6666ca36d2d35dc0f0e0ef1ec3ddcbcc9aa363
mxjwkv <mxjwkv@gmail.com>
2015-08-28 16:34:38 -0700

    Change libclass lib name

--- build_libclass.bat
+++ build_libclass.bat
@@ -1,7 +1,7 @@
 ::build_libclass.bat
 ::------------------------
 ::    Created: 26-AUG-2015
-::Last Update: 26-AUG-2015
+::Last Update: 27-AUG-2015
 ::
 ::WINDOWS
 @echo off
@@ -10,13 +10,13 @@
 echo --- Release --------------------------------------------------------
 ..\..\__incrbuildnum __build_num_release -L
 cl *.cpp /O2 /MT /c /EHsc /W3
-lib *.obj /OUT:..\lib\libclass.lib
-lib /LIST:..\lib\libclass.txt ..\lib\libclass.lib
-echo libclass.lib contains:
-type ..\lib\libclass.txt
+lib *.obj /OUT:..\lib\wlp_libclass.lib
+lib /LIST:..\lib\wlp_libclass.txt ..\lib\wlp_libclass.lib
+echo wlp_libclass.lib contains:
+type ..\lib\wlp_libclass.txt
 :: -------------------------------------
 :: Build DEBUG Lib - Multi-Threaded
 echo --- Debug ----------------------------------------------------------
 ..\..\__incrbuildnum __build_num_debug -L
 cl *.cpp /D "_DEBUG" /MTd /O2 /c /EHsc /W3
-lib *.obj /OUT:..\lib\libclassD.lib
+lib *.obj /OUT:..\lib\wlp_libclassD.lib
